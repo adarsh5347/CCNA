178069cecc7af353acc33a2026fd16f040842ebf
Adarsh Poojari <adarshpoojari8630@gmil.com>
2026-06-10 21:02:18 +0530

    config(auth): apply active Firebase project API keys

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,10 +1,11 @@
 // Firebase project configuration credentials
 // Copy these values from the Project Settings in your Firebase Console
 export const firebaseConfig = {
-  apiKey: "YOUR_FIREBASE_API_KEY",
-  authDomain: "YOUR_FIREBASE_AUTH_DOMAIN",
-  projectId: "YOUR_FIREBASE_PROJECT_ID",
-  storageBucket: "YOUR_FIREBASE_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_FIREBASE_MESSAGING_SENDER_ID",
-  appId: "YOUR_FIREBASE_APP_ID"
+  apiKey: "AIzaSyA9SZdtWUxecIuc9-ElizaIHTEiw648D6o",
+  authDomain: "ccna-prep-platform.firebaseapp.com",
+  projectId: "ccna-prep-platform",
+  storageBucket: "ccna-prep-platform.firebasestorage.app",
+  messagingSenderId: "738402326601",
+  appId: "1:738402326601:web:2fb22f32441b0856174798",
+  measurementId: "G-BEVQDX14EQ"
 };
